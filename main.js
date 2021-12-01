153e1abed4c6fad6e6de64ed9c9912bd9c666be1
potterhead0101 <82883344+potterhead0101@users.noreply.github.com>
2021-12-01 19:30:01 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+var canvas = new fabric.Canvas('myCanvas');
+ var x= document.getElementById("myAudio");
+
+function new_image()
+{
+    fabric. Image. fromURL (get_image, function(Img) {
+        
+	
+}
+
+function playSound(){
+    x.playSound("bdayaudio.mp3")
+}
